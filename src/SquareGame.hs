494a4847d357b03ec3aa68f154743195f8708006
jasonincanada <nirgle@gmail.com>
2019-04-11 17:46:41 -0400

    decouple CellBorder from Cell, add contigs/next/expand

--- src/SquareGame.hs
+++ src/SquareGame.hs
@@ -2,6 +2,7 @@
 
 module SquareGame where
 
+import Data.Bifunctor (bimap)
 import qualified Data.Map as M
 import qualified Data.Set as S
 
@@ -9,17 +10,18 @@ import qualified Data.Set as S
 {--- Types ---}
 
 
--- I'll likely optimize how the veiled/unveiled squares are tracked but for now this is
--- conceptually easy to understand
-data Board   = Board { squares  :: S.Set Square   -- All squares on this board
-                     , unveiled :: S.Set Square   -- Fully unshrouded squares
-                     , veiled   :: S.Set Square   -- At least one cell still shrouded
+data Board   = Board { -- Map a square to its shrouded and unshrouded grid cells
+                       squares :: M.Map Square (S.Set Cell, S.Set Cell)
+
+                       -- Map from grid cell to the square it belongs to and the cell's border type
+                     , grid :: M.Map Cell (Square, CellBorder)
+
                      } deriving (Show)
 
-type Square  = (Row, Col, Size)
-type Row     = Int
-type Col     = Int
-type Size    = Int
+type Square = (Row, Col, Size)
+type Row    = Int
+type Col    = Int
+type Size   = Int
 
 data SquareSide = STop | SRight | SBottom | SLeft
                   deriving (Show)
@@ -30,10 +32,8 @@ data SquareSide = STop | SRight | SBottom | SLeft
 -- full length of the other side of the edge isn't revealed since we don't want to identify
 -- whether there's a square side coincident with the current square (other than the side being
 -- clicked obviously)
-type Cell     = (CellRow, CellCol, CellBorder)
-type CellRow  = Int
-type CellCol  = Int
-type Shrouded = Bool
+type Cell = Pos
+type Pos  = (Row, Col)
 
 -- A cell has one and only one of these border types
 data CellBorder = CTopLeft
@@ -51,7 +51,7 @@ data CellBorder = CTopLeft
 {--- Square<->Cell maps ---}
 
 -- Map a square's side to the cells interior to the square that contact that side
-borderCells :: Square -> SquareSide -> [(CellRow, CellCol)]
+borderCells :: Square -> SquareSide -> [Cell]
 borderCells (row, col, size) = \case
   STop    -> [ (r,      c+i   ) | i <- [0..int] ]
   SBottom -> [ (r+s2-1, c+i   ) | i <- [0..int] ]
@@ -65,13 +65,13 @@ borderCells (row, col, size) = \case
 
 
 -- Map a square's side to the cells revealed when clicking that side, which is almost the whole
--- side but not the very ends, this keeps any incident edges shrouded
+-- side but not the very ends, this keeps any coincident edges shrouded
 click :: Square -> SquareSide -> [Cell]
 click (row, col, size) = \case
-  STop    -> [ (r-1,  c+i,  CBottom) | i <- [1..int] ]
-  SBottom -> [ (r+s2, c+i,  CTop   ) | i <- [1..int] ]
-  SLeft   -> [ (r+i,  c-1,  CRight ) | i <- [1..int] ]
-  SRight  -> [ (r+i,  c+s2, CLeft  ) | i <- [1..int] ]
+  STop    -> [ (r-1,  c+i ) | i <- [1..int] ]
+  SBottom -> [ (r+s2, c+i ) | i <- [1..int] ]
+  SLeft   -> [ (r+i,  c-1 ) | i <- [1..int] ]
+  SRight  -> [ (r+i,  c+s2) | i <- [1..int] ]
   where
     c    = col  * 2
     r    = row  * 2
@@ -80,7 +80,7 @@ click (row, col, size) = \case
 
 
 -- Map a square to its interior cells
-cells :: Square -> [Cell]
+cells :: Square -> [(Cell, CellBorder)]
 cells (row, col, size) = topleft ++ top ++ topright ++
                          left ++ interior ++ right ++
                          bottomleft ++ bottom ++ bottomright
@@ -94,16 +94,48 @@ cells (row, col, size) = topleft ++ top ++ topright ++
     cz   = c + int + 1
     int  = 2 * size - 2
 
-    topleft     = [ (r, c,     CTopLeft)                 ]
-    top         = [ (r, c+i,   CTop)     | i <- [1..int] ]
-    topright    = [ (r, cz,    CTopRight)                ]
+    topleft     = [ ((r, c  ),   CTopLeft)                 ]
+    top         = [ ((r, c+i),   CTop)     | i <- [1..int] ]
+    topright    = [ ((r, cz ),   CTopRight)                ]
 
-    left        = [ (r+i, c,   CLeft)    | i <- [1..int] ]
-    interior    = [ (r+i, c+j, CNone)    | i <- [1..int],
-                                           j <- [1..int] ]
-    right       = [ (r+i, cz,  CRight)   | i <- [1..int] ]
+    left        = [ ((r+i, c  ), CLeft)    | i <- [1..int] ]
+    interior    = [ ((r+i, c+j), CNone)    | i <- [1..int],
+                                             j <- [1..int] ]
+    right       = [ ((r+i, cz),  CRight)   | i <- [1..int] ]
 
-    bottomright = [ (rz,  cz,  CBottomRight)             ]
-    bottom      = [ (rz,  c+i, CBottom)  | i <- [1..int] ]
-    bottomleft  = [ (rz,  c,   CBottomLeft)              ]
+    bottomright = [ ((rz,  cz ), CBottomRight)             ]
+    bottom      = [ ((rz,  c+i), CBottom)  | i <- [1..int] ]
+    bottomleft  = [ ((rz,  c  ), CBottomLeft)              ]
+
+
+-- Get the next coordinate when traversing the cells along a square's side
+next :: SquareSide -> Pos -> Pos
+next STop    = bimap id (+1)
+next SBottom = bimap id (+1)
+next SLeft   = bimap (+1) id
+next SRight  = bimap (+1) id
+
+
+type NextPos = Pos -> Pos
+
+-- Group coordinates into the position and length of their contiguous chunks.
+-- This function assumes the list is already ordered
+contigs :: NextPos -> [Pos] -> [(Pos, Int)]
+contigs _    []     = []
+contigs next (a:as) = (a, count) : contigs next rest
+  where
+    count = length $ takeWhile (uncurry (==))
+                   $ zip (a:as) ray
+    ray   = iterate next a
+    rest  = (drop (count-1) as)
+
+
+-- Map a position and length returned from contigs to all the positions we can deshroud
+expand :: (Pos, Int) -> SquareSide -> [Pos]
+expand ((row, col), n) = \case
+  STop    -> [ (row+r, col+c) | r <- [0..n-1], c <- [0..n-1]]
+  SBottom -> [ (row-r, col+c) | r <- [0..n-1], c <- [0..n-1]]
+  SLeft   -> [ (row+r, col+c) | r <- [0..n-1], c <- [0..n-1]]
+  SRight  -> [ (row+r, col-c) | r <- [0..n-1], c <- [0..n-1]]
+
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,58 +8,102 @@ main = hspec $ do
 
   describe "borderCells computes the cells for..." $ do
     it "top border" $
-      borderCells (0,0,2) STop    `shouldBe` ( [(0,0),(0,1),(0,2),(0,3)] :: [(CellRow, CellCol)] )
+      borderCells (0,0,2) STop    `shouldBe` ( [(0,0),(0,1),(0,2),(0,3)] :: [Cell] )
 
     it "right border" $
-      borderCells (0,0,2) SRight  `shouldBe` ( [(0,3),(1,3),(2,3),(3,3)] :: [(CellRow, CellCol)] )
+      borderCells (0,0,2) SRight  `shouldBe` ( [(0,3),(1,3),(2,3),(3,3)] :: [Cell] )
 
     it "bottom border" $
-      borderCells (0,0,2) SBottom `shouldBe` ( [(3,0),(3,1),(3,2),(3,3)] :: [(CellRow, CellCol)] )
+      borderCells (0,0,2) SBottom `shouldBe` ( [(3,0),(3,1),(3,2),(3,3)] :: [Cell] )
 
     it "left border" $
-      borderCells (0,0,2) SLeft   `shouldBe` ( [(0,0),(1,0),(2,0),(3,0)] :: [(CellRow, CellCol)] )
+      borderCells (0,0,2) SLeft   `shouldBe` ( [(0,0),(1,0),(2,0),(3,0)] :: [Cell] )
 
 
   describe "click computes the cells to reveal for side..." $ do
     it "top side" $
-      click (2,4,2) STop    `shouldBe` ( [(3,9,CBottom),(3,10,CBottom)] :: [(CellRow, CellCol, CellBorder)] )
+      click (2,4,2) STop    `shouldBe` ( [(3,9),(3,10)]  :: [Cell] )
 
     it "right side" $
-      click (2,4,2) SRight  `shouldBe` ( [(5,12,CLeft),(6,12,CLeft)]    :: [(CellRow, CellCol, CellBorder)] )
+      click (2,4,2) SRight  `shouldBe` ( [(5,12),(6,12)] :: [Cell] )
 
     it "bottom side" $
-      click (2,4,2) SBottom `shouldBe` ( [(8,9,CTop),(8,10,CTop)]       :: [(CellRow, CellCol, CellBorder)] )
+      click (2,4,2) SBottom `shouldBe` ( [(8,9),(8,10)]  :: [Cell] )
 
     it "left side" $
-      click (2,4,2) SLeft   `shouldBe` ( [(5,7,CRight),(6,7,CRight)]    :: [(CellRow, CellCol, CellBorder)] )
+      click (2,4,2) SLeft   `shouldBe` ( [(5,7),(6,7)]   :: [Cell] )
 
 
   describe "cells maps a square to its cells" $ do
     it "1x1 square in top-left corner" $
-      cells (0,0,1) `shouldBe` ( [(0,0,CTopLeft),(0,1,CTopRight),(1,0,CBottomLeft),(1,1,CBottomRight)] :: [Cell] )
+      cells (0,0,1) `shouldBe` ( [((0,0),CTopLeft    ),
+                                  ((0,1),CTopRight   ),
+                                  ((1,0),CBottomLeft ),
+                                  ((1,1),CBottomRight)] :: [(Cell,CellBorder)] )
 
     it "3x3 square at row 2, column 4" $
       cells (2,4,3) `shouldBe`
         ( [ -- top
-            (4,8,CTopLeft),(4,9,CTop),(4,10,CTop),(4,11,CTop),(4,12,CTop),(4,13,CTopRight),
+            ((4,8),CTopLeft),((4,9),CTop),((4,10),CTop),((4,11),CTop),((4,12),CTop),((4,13),CTopRight),
 
             -- left side
-            (5,8,CLeft),(6,8,CLeft),(7,8,CLeft),(8,8,CLeft),
+            ((5,8),CLeft),((6,8),CLeft),((7,8),CLeft),((8,8),CLeft),
 
             -- interior cells with no border
-            (5,9,CNone),(5,10,CNone),(5,11,CNone),(5,12,CNone),
-            (6,9,CNone),(6,10,CNone),(6,11,CNone),(6,12,CNone),
-            (7,9,CNone),(7,10,CNone),(7,11,CNone),(7,12,CNone),
-            (8,9,CNone),(8,10,CNone),(8,11,CNone),(8,12,CNone),
+            ((5,9),CNone),((5,10),CNone),((5,11),CNone),((5,12),CNone),
+            ((6,9),CNone),((6,10),CNone),((6,11),CNone),((6,12),CNone),
+            ((7,9),CNone),((7,10),CNone),((7,11),CNone),((7,12),CNone),
+            ((8,9),CNone),((8,10),CNone),((8,11),CNone),((8,12),CNone),
 
             -- right side
-            (5,13,CRight),(6,13,CRight),(7,13,CRight),(8,13,CRight),
+            ((5,13),CRight),((6,13),CRight),((7,13),CRight),((8,13),CRight),
 
             -- bottom
-            (9,8,CBottomLeft),(9,9,CBottom),(9,10,CBottom),(9,11,CBottom),(9,12,CBottom),(9,13,CBottomRight) ]
-            :: [Cell]
+            ((9,8),CBottomLeft),((9,9),CBottom),((9,10),CBottom),((9,11),CBottom),((9,12),CBottom),((9,13),CBottomRight) ]
+            :: [(Cell,CellBorder)]
         )
 
     it "returns 5184 cells for a square-sized sub-square" $
       length (cells (0,0,36)) `shouldBe` (36*36*4)
+
+
+  describe "contigs" $ do
+    it "on empty list" $
+      contigs (next STop) [] `shouldBe` []
+
+    it "on singleton list" $
+      contigs (next STop) [(0,0)] `shouldBe` [((0,0),1)]
+
+    it "on top side cells" $
+      contigs (next STop) [(0,0),(0,1),(0,2), (0,100),(0,101), (0,200)] 
+        `shouldBe` [ ((0,0  ), 3),
+                     ((0,100), 2),
+                     ((0,200), 1)
+                   ]
+
+    it "on left side cells" $
+      contigs (next SLeft) [(0,0),(1,0),(2,0), (100,0),(101,0), (200,0)] 
+        `shouldBe` [ ((0,0  ), 3),
+                     ((100,0), 2),
+                     ((200,0), 1)
+                   ]
+
+
+  describe "expand" $ do
+    it "top side 3-cell deshroud" $
+      expand ((0,0), 3) STop    `shouldBe` [ (0,0), (0,1), (0,2),
+                                             (1,0), (1,1), (1,2),
+                                             (2,0), (2,1), (2,2) ]
+    it "bottom side 3-cell deshroud" $
+      expand ((2,0), 3) SBottom `shouldBe` [ (2,0), (2,1), (2,2),
+                                             (1,0), (1,1), (1,2),
+                                             (0,0), (0,1), (0,2) ]
+    it "left side 3-cell deshroud" $
+      expand ((0,0), 3) SLeft   `shouldBe` [ (0,0), (0,1), (0,2),
+                                             (1,0), (1,1), (1,2),
+                                             (2,0), (2,1), (2,2) ]
+    it "right side 3-cell deshroud" $
+      expand ((0,2), 3) SRight  `shouldBe` [ (0,2), (0,1), (0,0),
+                                             (1,2), (1,1), (1,0),
+                                             (2,2), (2,1), (2,0) ]
 
