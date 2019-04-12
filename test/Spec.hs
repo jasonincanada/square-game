10017e91c5787ccd5fbfaae1803f76eec449387a
jasonincanada <nirgle@gmail.com>
2019-04-11 20:12:25 -0400

    correct wording

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,7 +5,7 @@ import qualified Data.Set as S
 import SquareGame
 import Test.Hspec
 
--- These would be read from a file/database. This is our mock board, a mini 4x6 grid that
+-- These would be read from a file/database. This is our mock board, a mini 4x6 board that
 -- has one 4x4 square to the left of two stacked 2x2 squares
 testSquares :: [Square]
 testSquares = [(0,0,4), (0,4,2), (2,4,2)]
@@ -118,7 +118,7 @@ main = hspec $ do
   describe "board" $ do
     let (Board squares cells) = board testSquares
 
-    it "maps squares to tiles" $
+    it "maps squares to grid cells" $
       squares `shouldBe` M.fromList [ ((0,0,4),( S.fromList [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7),(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(2,6),(2,7),(3,0),(3,1),(3,2),(3,3),(3,4),(3,5),(3,6),(3,7),(4,0),(4,1),(4,2),(4,3),(4,4),(4,5),(4,6),(4,7),(5,0),(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(6,0),(6,1),(6,2),(6,3),(6,4),(6,5),(6,6),(6,7),(7,0),(7,1),(7,2),(7,3),(7,4),(7,5),(7,6),(7,7)]
                                                , S.fromList []))
                                     , ((0,4,2),( S.fromList [(0,8),(0,9),(0,10),(0,11),(1,8),(1,9),(1,10),(1,11),(2,8),(2,9),(2,10),(2,11),(3,8),(3,9),(3,10),(3,11)]
@@ -133,6 +133,6 @@ main = hspec $ do
     it "maps the last cell to the last square" $
       cells M.! (7,11) `shouldBe` ((2,4,2), CBottomRight)
 
-    it "maps the right number of cells" $
+    it "maps to the right number of cells" $
       M.size cells `shouldBe` 4*(4*4 + 2*2 + 2*2)
 
